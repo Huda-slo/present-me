23d6fd01b954ce0a5c8a5b9b9c5c9aef718708f8
maha3i <mmahhaa1999@gmail.com>
2024-09-29 11:24:55 +0300

    maha

---
+++ TEST2/maha.swift
@@ -0,0 +1,18 @@
+//
+//  maha.swift
+//  TEST2
+//
+//  Created by maha on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct maha: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    maha()
+}
