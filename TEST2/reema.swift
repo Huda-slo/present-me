d53fe27ed1d5461d23906df66f696d391fa99b77
Reema-M0 <Reemaal200200@gmil.com>
2024-09-29 01:29:51 -0700

    reema

---
+++ TEST2/reema.swift
@@ -0,0 +1,18 @@
+//
+//  reema.swift
+//  TEST2
+//
+//  Created by Reem on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct reema: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    reema()
+}
