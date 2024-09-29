58869ac3b9bd4292b441955b29af68b1174e8690
Arafat-Alsaieq <a.h.b.alsaieq@gmail.com>
2024-09-29 11:37:03 +0300

    Arafat Update

--- TEST2/arafat.swift
+++ TEST2/arafat.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 struct arafat: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+           // Color.gray
+              //  .ignoresSafeArea()
+                        
+            Image("ed6aaa685737ad811ef42881507af4c1")
+            Text("Arafat Alsaieq")
+        
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color.purple)
+                .multilineTextAlignment(.leading)
+            Text("Arafat,has recently joined the Apple Developer Academy. She has a strong interest in coding and UI/UX design, showing a passion for technology and user-centered design. This new journey is set to help her further develop her skills in app development and enhance her creativity in creating intuitive digital experiences.")
+            
+          
+            }
+        .padding()
+       
+            
+        }
     }
-}
+
 
 #Preview {
     arafat()
