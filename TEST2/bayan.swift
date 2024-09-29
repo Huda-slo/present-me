fee1db2a3ebd067a1fc65480f7d8db66d20d50a4
BayanADA <Bayanalshuwaier@gmail.com>
2024-09-29 11:15:42 +0300

    bayan update

--- TEST2/bayan.swift
+++ TEST2/bayan.swift
@@ -5,4 +5,46 @@
 //  Created by Bayan Alshuwaier on 29/09/2024.
 //
 
-import Foundation
+
+import SwiftUI
+
+struct bayan: View {
+    var body: some View {
+        VStack {
+            
+            HStack {
+                Image("riyadh")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                .cornerRadius(30)
+                VStack .init(alignment: .leading)
+                {
+                    Text("Riyadh")
+                        .foregroundColor(.black)
+                        .font(.largeTitle)
+                        .multilineTextAlignment(.leading)
+                    Text("Capital of Saudi Arabia")
+                        .foregroundColor(.gray)
+                        .multilineTextAlignment(.leading)
+                    
+                }
+                Text("🇸🇦")
+                    
+                    
+                    
+                    
+            }
+            
+        
+            
+            }
+            
+        .padding()
+        
+         
+        }
+    }
+
+#Preview {
+    bayan()
+}
